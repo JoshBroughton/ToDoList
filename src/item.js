4422647bb03d9e7ce6df24ef25a90110e59a437a
Joshua Broughton <josh.broughton47@gmail.com>
2022-01-18 20:36:55 -0700

    finished DOM function for creating new item submission form

---
+++ src/item.js
@@ -0,0 +1,17 @@
+let itemFactory = (title, description, dueDate, priority, project = "none") => {
+    let title = title;
+    let description = description;
+    let dueDate = dueDate;
+    let priority = priority;
+    let project = project;
+    //setters for editing tasks
+    let setTitle = (newTitle) => title = newTitle;
+    let setDescription = (newDescription) => description = newDescription;
+    let setDueDate = (newDueDate) => dueDate = newDueDate;
+    let setPriority = (newPriority) => priority = newPriority;
+    let setProject = (newProject) => project = newProject;
+
+    return { title, description, dueDate, priority, project, 
+        setTitle, setDescription, setDueDate, setPriority, setPriority}
+}
+export default itemFactory;
---
+++ src/newItemDOM.js
@@ -0,0 +1,43 @@
+let newItemDOM = (title = "", description = "", dueDate = "", priority = "", project = "none") => {
+    //create all the elements for inputting the fields to create a new item object
+    let container = document.createElement("DIV");
+    let textInput = document.createElement("FORM");
+    let titleLabel = document.createElement("LABEL");
+    let titleIn = document.createElement("INPUT");
+    let descriptionLabel = document.createElement("LABEL");
+    let descriptionIn = document.createElement("INPUT");
+    let dueDateIn = document.createElement("BUTTON");
+    let projectIn = document.createElement("BUTTON");
+    let priorityIn = document.createElement("BUTTON");
+    let submit = document.createElement("BUTTON");
+    let cancel = document.createElement("BUTTON");
+    //set basic attributes
+    titleLabel.setAttribute("for", "title");
+    titleIn.setAttribute("type", "text");
+    titleIn.setAttribute("id", "titleIn");
+    titleIn.setAttribute("name", "titleIn");
+
+    descriptionLabel.setAttribute("for", "description");
+    descriptionIn.setAttribute("type", "text");
+    descriptionIn.setAttribute("id", "descriptionIn");
+    descriptionIn.setAttribute("name", "descriptionIn");
+
+    dueDateIn.setAttribute("type", "button");
+    dueDateIn.setAttribute("id", "dueDateIn")
+    dueDateIn.innerText = "Due Date";
+    projectIn.setAttribute("type", "button");
+    projectIn.setAttribute("id", "projectIn");
+    projectIn.innerText = "Project";
+    priorityIn.setAttribute("type", "button");
+    priorityIn.setAttribute("id", "priorityIn");
+    priorityIn.innerText = "Priority";
+
+    submit.setAttribute("type", "button");
+    submit.setAttribute("id", "submit");
+    submit.innerText = "Submit";
+    cancel.setAttribute("type", "button");
+    cancel.setAttribute("id", "cancel");
+    cancel.innerText = "Cancel";
+}
+
+export default newItemDOM;
