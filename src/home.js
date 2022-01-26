fb72de8b3a1134e45ac61cf5982cb5ee7f57f237
Joshua Broughton <josh.broughton47@gmail.com>
2022-01-25 21:48:00 -0700

    finished static DOM elements of main page

--- src/home.js
+++ src/home.js
@@ -43,10 +43,43 @@ let home = () => {
 
     //define sidebar elements
     let onDeckButton = document.createElement("BUTTON");
-    
+    let stagingButton = document.createElement("BUTTON");
     let projectHeader = document.createElement("H1");
+    //projects will need to be dynamically appended the the header
+    onDeckButton.setAttribute("id", "onDeckButton");
+    onDeckButton.setAttribute("type", "button");
+    onDeckButton.innerText = "On Deck";
+    stagingButton.setAttribute("id", "stagingButton");
+    stagingButton.setAttribute("type", "button");
+    stagingButton.innerText = "Staging";
+    projectHeader.setAttribute("id", "projectHeader");
+    projectHeader.innerText = "Projects"
 
+    //append to sidebar
+    sidebar.appendChild(onDeckButton);
+    sidebar.appendChild(stagingButton);
+    sidebar.appendChild(projectHeader);
 
+    //temporary elements to get css roughed in before working on dynamic project adding
+    let tempSubHead = document.createElement("H1");
+    let tempContent = document.createElement("P");
+
+    tempSubHead.innerText = "SUBHEADER";
+    tempContent.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "
+
+    //rough in footer elements
+    let footerStatement = document.createElement("H1");
+    let footerButton = document.createElement("BUTTON");
+
+    footerStatement.setAttribute("id", "footerStatement");
+    footerStatement.innerText = "Josh Broughton";
+    footerButton.setAttribute("id", "footerButton");
+    footerButton.setAttribute("type", "button");
+
+    subHeader.appendChild(tempSubHead);
+    content.appendChild(tempContent);
+    footer.appendChild(footerStatement);
+    footer.appendChild(footerButton);
 
     return container;
 }
