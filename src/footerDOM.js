c3618f3835d9fa4c3652e3b84b879cd8b946e82e
Joshua Broughton <josh.broughton47@gmail.com>
2022-02-02 22:08:34 -0700

    refactored footerDOM into its own module

---
+++ src/footerDOM.js
@@ -0,0 +1,20 @@
+let footer = () => {
+    let footer = document.createElement("DIV");
+    footer.setAttribute("id", "footer");
+
+    //rough in footer elements
+    let footerStatement = document.createElement("H1");
+    let footerButton = document.createElement("BUTTON");
+
+    footerStatement.setAttribute("id", "footerStatement");
+    footerStatement.innerText = "Josh Broughton";
+    footerButton.setAttribute("id", "footerButton");
+    footerButton.setAttribute("type", "button");
+    footerButton.innerText = "Github";
+
+    footer.appendChild(footerStatement);
+    footer.appendChild(footerButton);
+
+    return footer;
+}
+export default footer;
--- src/home.js
+++ src/home.js
@@ -2,6 +2,7 @@ import header from './headerDOM';
 import sidebar from './sidebarDOM';
 import subHeader from './subHeadDOM';
 import content from './contentDOM';
+import footer from './footerDOM';
 
 let home = () => {
     //define container and grid children
@@ -9,20 +10,20 @@ let home = () => {
     
     
     
-    let footer = document.createElement("DIV");
+    
 
     container.setAttribute("id", "gridContainer");
     
     
     
-    footer.setAttribute("id", "footer");
+    
 
     //append children to grid container
     container.appendChild(header());
     container.appendChild(sidebar());
     container.appendChild(subHeader());
     container.appendChild(content());
-    container.appendChild(footer);
+    container.appendChild(footer());
 
 
     
@@ -33,20 +34,7 @@ let home = () => {
 
     
 
-    //rough in footer elements
-    let footerStatement = document.createElement("H1");
-    let footerButton = document.createElement("BUTTON");
-
-    footerStatement.setAttribute("id", "footerStatement");
-    footerStatement.innerText = "Josh Broughton";
-    footerButton.setAttribute("id", "footerButton");
-    footerButton.setAttribute("type", "button");
-    footerButton.innerText = "Github";
-
     
-    
-    footer.appendChild(footerStatement);
-    footer.appendChild(footerButton);
 
     return container;
 }
