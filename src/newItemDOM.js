61599c9bb10b2a8d39e94164d94c8a0f26e80687
Joshua Broughton <josh.broughton47@gmail.com>
2022-02-16 18:52:21 -0700

    working on form styling

--- src/newItemDOM.js
+++ src/newItemDOM.js
@@ -1,9 +1,7 @@
 let newItemDOM = (title = "", description = "", dueDate = "", priority = "", project = "none") => {
     //create all the elements for inputting the fields to create a new item object
     let textInput = document.createElement("FORM");
-    let titleLabel = document.createElement("LABEL");
     let titleIn = document.createElement("INPUT");
-    let descriptionLabel = document.createElement("LABEL");
     let descriptionIn = document.createElement("TEXTAREA");
     let dueDateIn = document.createElement("BUTTON");
     let projectIn = document.createElement("BUTTON");
@@ -11,20 +9,18 @@ let newItemDOM = (title = "", description = "", dueDate = "", priority = "", pro
     let submit = document.createElement("BUTTON");
     let cancel = document.createElement("BUTTON");
 
-    textInput.setAttribute("id", "")
+    textInput.setAttribute("id", "form")
     
     //set basic attributes
-    titleLabel.setAttribute("for", "title");
-    titleLabel.innerText = "Task Title";
     titleIn.setAttribute("type", "text");
     titleIn.setAttribute("id", "titleIn");
     titleIn.setAttribute("name", "titleIn");
+    titleIn.setAttribute("placeholder", 'Task Title');
 
-    descriptionLabel.setAttribute("for", "description");
-    descriptionLabel.innerText = "Description";
     descriptionIn.setAttribute("type", "text");
     descriptionIn.setAttribute("id", "descriptionIn");
     descriptionIn.setAttribute("name", "descriptionIn");
+    descriptionIn.setAttribute('placeholder', 'Description');
 
     dueDateIn.setAttribute("type", "button");
     dueDateIn.setAttribute("id", "dueDateIn")
@@ -43,9 +39,7 @@ let newItemDOM = (title = "", description = "", dueDate = "", priority = "", pro
     cancel.setAttribute("id", "cancel");
     cancel.innerText = "Cancel";
     //append all sub items to the div and form
-    textInput.appendChild(titleLabel);
     textInput.appendChild(titleIn);
-    textInput.appendChild(descriptionLabel);
     textInput.appendChild(descriptionIn);
     textInput.appendChild(dueDateIn);
     textInput.appendChild(projectIn);
